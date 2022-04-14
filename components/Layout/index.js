84133a50919ef96d735de7ff9ca05070eab6a6a2
Rohyller <rohyler@gmail.com>
2022-04-14 02:20:38 -0400

    Enviado por segunda vez

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,29 +1,45 @@
-import { 
-Box,
+import {
+	Box,
 } from "@chakra-ui/react";
 import Navbar from "./navbar.jsx";
 import Footer from "./footer.jsx";
 import Head from "next/head";
 
-export default function Layout({children,title,description}){
+export default function Layout({ children, title, description }) {
 	return (
 		<Box>
-	      <Head>
-	        <title>{title}</title>
-	        <meta charSet="utf-8"/> 
-	        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-	        <link rel="preconnect" href="https://fonts.googleapis.com" />
-			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-			<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
-		  </Head>
-	      <Navbar />
-	      <main>{children}</main>
-	      <Footer />
-	    </Box>
+			<Head>
+				<title>{title}</title>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+				<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<meta name="keywords" content="Colegio Maria Santisima portal web sitio web" />
+				<meta name="description" content="Diseño de un portal web como alternativa de solución para mejorar la Calidad
+                evaluativa online del docente de 5to A" />
+				<meta name="author" content="Rohyller Pereira" />
+				<meta name="copyright" content="Rohyller Pereira @2022" />
+				<meta property="og:image" content="/img/logo.jpg" />
+				<meta property="og:description" content="iseño de un portal web como alternativa de solución para mejorar la Calidad
+                evaluativa online del docente" />
+				<meta property="og:title" content="Colegio portal web" />
+				<meta property="twitter:image" content="/img/logo.jpg" />
+				<meta property="twitter:description" content="Diseño de un portal web como alternativa de solución para mejorar la Calidad
+                evaluativa online del docente de 5to A" />
+				<meta property="twitter:title" content="Colegio portal web" />
+				<link rel="shortcut icon" href="/ico/logo.ico"/>
+			</Head>
+			<Navbar />
+			<main>{children}</main>
+			<Footer />
+		</Box>
 	)
 }
 
 Layout.defaultProps = {
-  title:"Inicio",
-  description: "Conociendo las emociones de los profesores del Colegio 'María Santísima' "
+	title: "Inicio",
+	description: "Conociendo las emociones de los profesores del Colegio 'María Santísima' "
 }
